c355bc6d3bf2aec065ad523563b35564a3fbac3d
peter <peter@felis.nl>
2025-02-18 16:48:03 +0100

    dinsdagmiddag op kantoor

--- incs/menu.php
+++ incs/menu.php
@@ -4,49 +4,48 @@
 // $menu = 'geen', 'normaal' of 'beheer', moet meegegeven worden bij aanroep
 
 if ($menu == 'normaal') { ?>
-    
-        <header>
-            <nav>
-                <a class="logo" href=" /">
-                    <img src="/afbeeldingen/fetumlogo.png" alt="Fetum logo" />
-                </a>
-                <a href="/onderwijs.php">Onderwijs</a>
-                <a href="/zorg.php">Zorg</a>
-                <a href="webshopinfo.php">Webshop Info</a>
-                <a href="contact.php">Contact</a>
-                <a href="shop.php">Webshop</a>
-                <a href="klantForm.php">Wordt klant</a>
-                <?php
 
-                if (!isset($_SESSION['user_id']) && (strtolower(basename($_SERVER['SCRIPT_NAME'], ".php")) !== 'loginform')) { ?>
-                    <a href="loginForm.php">Inloggen</a>
-                <?php } ?>
+    <header>
+        <nav>
+            <a class="logo" href=" /">
+                <img src="/afbeeldingen/fetumlogo.png" alt="Fetum logo" />
+            </a>
+            <a href="/onderwijs.php">Onderwijs</a>
+            <a href="/zorg.php">Zorg</a>
+            <a href="webshopinfo.php">Webshop Info</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Webshop</a>
+            <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'klantForm') echo '<a href="klantForm.php">Wordt klant</a>';
 
-            </nav>
-        </header>
-  
+            if (!isset($_SESSION['user_id']) && (strtolower(basename($_SERVER['SCRIPT_NAME'], ".php")) !== 'loginform')) { ?>
+                <a href="loginForm.php">Inloggen</a>
+            <?php } ?>
+
+        </nav>
+    </header>
+
 <?php } else if ($menu == 'beheer') { ?>
-   
-        <header>
-            <nav>
-                <a class="logo" href=" /">
-                    <img src="/afbeeldingen/fetumlogo.png" alt="Fetum logo" />
-                </a>
-                <?php
-                if (!isset($_SESSION['user_id']) && (strtolower(basename($_SERVER['SCRIPT_NAME'], ".php")) !== 'loginform')) { ?>
-                    <a href="loginForm.php">Inloggen</a>
-                <?php } ?>
-                <a href="logout.php">Logout</a>
-                <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'dashboard') echo '<a href="dashboard.php">Dashboard</a>'; ?>
-                <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'producten_beheer') echo '<a href="producten_beheer.php">Productenbeheer</a>'; ?>
-                <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'product_sticker') echo '<a href="product_sticker.php">Sticker Afdrukken</a>'; ?>
-                <a href="klantForm.php">Registreren</a>
-                <a href="contact.php">Contact</a>
-                <a href="leveringsvoorwaarden.php">Leveringsvoorwaarden</a>
-                <a href="webshopinfo.php">Webshopinfo</a>
-            </nav>
-        </header>
-   
+
+    <header>
+        <nav>
+            <a class="logo" href=" /">
+                <img src="/afbeeldingen/fetumlogo.png" alt="Fetum logo" />
+            </a>
+            <?php
+            if (!isset($_SESSION['user_id']) && (strtolower(basename($_SERVER['SCRIPT_NAME'], ".php")) !== 'loginform')) { ?>
+                <a href="loginForm.php">Inloggen</a>
+            <?php } ?>
+            <a href="logout.php">Logout</a>
+            <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'dashboard') echo '<a href="dashboard.php">Dashboard</a>'; ?>
+            <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'producten_beheer') echo '<a href="producten_beheer.php">Productenbeheer</a>'; ?>
+            <?php if (basename($_SERVER['SCRIPT_NAME'], ".php") !== 'product_sticker') echo '<a href="product_sticker.php">Sticker Afdrukken</a>'; ?>
+            <a href="klantForm.php">Registreren</a>
+            <a href="contact.php">Contact</a>
+            <a href="leveringsvoorwaarden.php">Leveringsvoorwaarden</a>
+            <a href="webshopinfo.php">Webshopinfo</a>
+        </nav>
+    </header>
+
 <?php } ?>
 
 <style>
--- shop.php
+++ shop.php
@@ -3,7 +3,7 @@ $title = "Fetum - Since 1985";
 $statusbalk = "Iets bestellen? Gewoon even mailen of bellen!";
 $eenpagina = true;
 include_once __dir__ . '/incs/top.php';
-include_once __dir__ . '/incs/product_selector.php';
+
 ?>
 
 
